efa41825e23fc03ff254ec224b650a444da8fd0f
Robin Lieb <robin.j.lieb@gmail.com>
2022-11-27 18:20:57 +0100

    ops: created terraform configuration

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.0.2"
+      }
+    }
+
+    required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg-dhbw-devops" {
+  name = "rg-dhbw-devops"
+  location = "eastus2"
+}
+
+resource "azurerm_service_plan" "app-service-plan" {
+  name = "plan-dhbw"
+  resource_group_name = azurerm_resource_group.rg-dhbw-devops.name
+  location = azurerm_resource_group.rg-dhbw-devops.location
+  sku_name = "F1"
+  os_type = "Linux"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name = "app-dhbw-test123"
+  resource_group_name = azurerm_resource_group.rg-dhbw-devops.name
+  location = azurerm_resource_group.rg-dhbw-devops.location
+  service_plan_id = azurerm_service_plan.app-service-plan.id
+
+  site_config {
+    always_on = false
+    application_stack {
+      docker_image = "robinlieb/dhbw"
+      docker_image_tag = "latest"
+    }
+  }
+}
